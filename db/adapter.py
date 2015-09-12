4de3d3a22ee024126ad9febedae31f79234798e1
Tatiana <tatianamolinari90@gmail.com>
2015-09-12 11:44:59 -0300

    cambiando querys para traer eventos y agregando guardar usuario

--- db/adapter.py
+++ db/adapter.py
@@ -53,7 +53,7 @@ class MongoDBAdapter(Adapter):
 				  "fecha":form.fecha.data,
 				  "descripcion":form.descripcion.data,
 				  "asistiran":0}
-		#db.eventos.insert_one(evento).inserted_id
+		db.eventos.insert_one(evento).inserted_id
 		db.usuarios.update({"_id": usuario_id},{'$push': {'eventos': {evento}}})
 
 
@@ -98,7 +98,7 @@ else:
 
 if __name__ == '__main__':
 
-	if 1==0:
+	if 1==1:
 		url = dbsettings.MONGO_DATABASE["url_conection"]
 		client = MongoClient(url)
 		db = client['prueba']
@@ -112,13 +112,38 @@ if __name__ == '__main__':
 		a=posts.find_one({"_id": post_id})
 		print a["hola"]
 		print type(a)
+		print "---------------------------------------------"
+		pos = {"name" : "MongoDB", "notes" : [ 
+		{"title" : "Hello MongoDBssa", "content" : "Hello MongoDBss" }, 
+		{"title" : "ReplicaSet MongoDB", "content" : "ReplicaSet MongoDB"} ] }
+		post_id = posts.insert_one(pos).inserted_id
+		a=posts.find_one({"_id": post_id})
+		print a
+		f = posts.find({ "notes" : { "$elemMatch" : { "title" : "Hello MongoDBssa"} }});
+		g = posts.find(
+			    {"notes.title": "Hello MongoDBssa"}, 
+			    {"_id": 0, "notes": {"$elemMatch": {"title": "Hello MongoDBssa"}}});
+		print f
+		for ss in f:
+			print ss
+		print "ssssssssssssssssssssss"
+		print g
+		for ssa in g:
+			print ssa
+		print "ssssssssssssssssssssss"
+		#print g.first()["notes"][0]
+
+		evento = {"organizador":"aaaa","asistiran":0}
+		print evento
+		db.eventos.insert_one(evento).inserted_id
+		print evento
 
 
 	usuario = Session().query(Usuario).first()
 
 	pa= json.loads(json.dumps(usuario, cls=AlchemyEncoder))
 
-	print pa
-	print pa["nombre"]
-	print type(pa)
+	#print pa
+	#print pa["nombre"]
+	#print type(pa)
 
