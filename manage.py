17e4063da71f6289f918bc0ec3016650b6fee65a
ruizdiazchristiand <ruizdiazchristiand@gmail.com>
2015-09-24 19:18:00 -0300

    Pequenio cambio de la configuracion de django

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db.dbsettings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orgeventos.db.dbsettings")
 
     from django.core.management import execute_from_command_line
 
