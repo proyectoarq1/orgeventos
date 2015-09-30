f30834e668ba04635a1df3cbd26381a92e582fda
Tatiana <tatianamolinari90@gmail.com>
2015-09-30 20:37:27 -0300

    agregando las invitaciones a ambos modelos de base de datos y transacciones que facilitan devolver los invitados de los eventos y los eventos a los que esta invitado un usuario

--- db/models.py
+++ db/models.py
@@ -13,16 +13,8 @@ from sqlalchemy_utils import database_exists, create_database
 
 import dbsettings
 
-#from flask.ext.social import Social
-#from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
-
-
 url = os.getenv('DATABASE_URL', URL(**dbsettings.DATABASE))
-print "*********************************** "
-print url
 engine = create_engine(url)
-print "*********************************** "
-print engine.url
 
 if (os.getenv('DATABASE_URL') is None) and not database_exists(engine.url):
 	print "INFO: Creating database ", dbsettings.DATABASE["database"]
@@ -48,6 +40,11 @@ class Evento(Base):
 	fecha = Column(DateTime)
 	asistiran = Column(Integer())
 	organizador_id = Column(Integer, ForeignKey('usuario.id'))
+
+class Invitacion(Base):
+	__tablename__ = 'invitacion'
+	evento_id = Column(Integer, primary_key=True)
+	usuario_id = Column(Integer, primary_key=True)
 
 
 if __name__ == '__main__':
