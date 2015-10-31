c70ec72d497b98498a0d0ce3447ce5dc4a2d36da
Tatiana <tatianamolinari90@gmail.com>
2015-10-31 13:55:28 -0300

    agregando cache con caducidad y algo de traduccion al uso de la api del tiempo closes #33

--- controllers/eventoController.py
+++ controllers/eventoController.py
@@ -3,29 +3,17 @@ from flask_restful import Resource
 from db.adapter_selected import adapter
 from flask import session,request
 from flask.ext.login import current_user
+from servicios.openWeatherMapAdapter import OpenWeatherMapAdapter
 import urllib2, json
 
-
-def get_clima(nombre_ciudad):
-
-    url = "http://api.openweathermap.org/data/2.5/weather?q="+nombre_ciudad+"&units=metric"+"&appid=713d008f823124ef813cfef342341934"
-
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
-    resultado = json.loads(response.read())
-    clima = {
-        'ubicacion': nombre_ciudad,
-        'cielo': resultado["weather"][0]["main"],
-        'descripcion': resultado["weather"][0]["description"],
-        'temperatura': resultado["main"]["temp"],
-        'icono' : resultado["weather"][0]["icon"]
-    }
-    return clima
+openweathermap = OpenWeatherMapAdapter()
 
 class EventoController(Resource):
+    
+
     def get(self,evento_id):
         evento = adapter.get_evento(evento_id)
-        clima_actual = get_clima(evento["ubicacion"])
+        clima_actual = openweathermap.get_clima(evento["ubicacion"])
         all_users = adapter.get_all_users()
         headers = {'Content-Type': 'text/html'}
         usuarios_invitados = adapter.obtener_usuarios_invitados_evento(evento_id)
@@ -36,7 +24,6 @@ class EventoController(Resource):
         current_app.logger.info('Agregando un invitado')
         invitado_id = request.form['invitado']
         adapter.crear_invitacion(evento_id,invitado_id)
-        #
         headers = {'Content-Type': 'text/html'}
         return redirect(url_for('evento', evento_id=evento_id))
 
---
+++ servicios/openWeatherMapAdapter.py
@@ -0,0 +1,55 @@
+import urllib2, json 
+
+from datetime import datetime, timedelta
+
+class OpenWeatherMapAdapter:
+
+    appid = "713d008f823124ef813cfef342341934"
+    cache = {}
+    cielo_traductor = {"clouds":"Nublado","clear":"Despejado", "rain":"Lluvias"}
+    descripcion_traductor = { 
+                             "sky is clear":"Cielo despejado, dia soleado", 
+                             "light rain":"Lluvias moderadas",
+                             "few clouds": "Pocas nubes",
+                             "scattered clouds": "Nubes espesas",
+                             "broken clouds":"Nubes de tormenta",
+                             "shower rain": "Lluvias copiosas",
+                             "rain": "Lluvias de moderadas a fuertes",
+                             "thunderstorm": "Tormentas de rayos",
+                             "snow": "Se esperan nevadas",
+                             "mist": "Probabilidad de neblina"
+              
+                             }
+    def caduco_valor_en_cache(self,fecha):
+        delta = datetime.now() - fecha
+        days, hours, minutes = delta.days, delta.seconds // 3600, delta.seconds // 60 % 60
+        return (days>1 or hours>8)
+
+    def obtener_clima(self,nombre_ciudad):
+        if nombre_ciudad in self.cache and not self.caduco_valor_en_cache(self.cache[nombre_ciudad]["fecha"]):
+            return self.cache[nombre_ciudad]["resultado"]
+        else:
+            resultado = self.hacer_request(nombre_ciudad)
+            resultado_cacheado = {"fecha": datetime.now(),"resultado": resultado}
+            self.cache[nombre_ciudad] = resultado_cacheado
+            return resultado
+
+    def hacer_request(self,nombre_ciudad):
+        url = "http://api.openweathermap.org/data/2.5/weather?q="+nombre_ciudad+"&units=metric"+"&appid="+ self.appid
+
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+        resultado = json.loads(response.read())
+        return resultado
+
+    def get_clima(self,nombre_ciudad):
+
+        resultado=self.obtener_clima(nombre_ciudad)
+        clima = {
+            'ubicacion': nombre_ciudad,
+            'cielo': self.cielo_traductor[resultado["weather"][0]["main"].lower()],
+            'descripcion': self.descripcion_traductor[resultado["weather"][0]["description"].lower()],
+            'temperatura': resultado["main"]["temp"],
+            'icono' : resultado["weather"][0]["icon"]
+        }
+        return clima
