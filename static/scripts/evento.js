cc5903e566c99b1a2680c9970509fbcd2c507579
ruizdiazchristiand <ruizdiazchristiand@gmail.com>
2015-11-23 21:55:03 -0300

    Agregando y los archivos correspondientes a jquery y jquery ui

--- static/scripts/evento.js
+++ static/scripts/evento.js
@@ -7,27 +7,6 @@ $(function() {
     }
 
 });
-function borrar_evento(id) {
-    console.log("delete is working!") // sanity check
-    
-    $.ajax({
-        url : "/editar/"+id,
-        type : "DELETE",
-        data : {id_evento:id}, 
-
-        success : function() {
-
-            window.location.replace("http://localhost:5000/perfil");
-        },
-
-        error : function(xhr,errmsg,err) {
-
-            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
-                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
-            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-        }
-    });
-};
 
 function invitar(evento_id){
     var opcionSeleccionada_id = $( "#invitado option:selected" ).val();
