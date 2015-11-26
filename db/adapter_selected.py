857a47467e92211179ee9b83c8efc0f584abc68f
Tatiana <tatianamolinari90@gmail.com>
2015-11-26 17:10:25 -0300

    cambiando dbselected

--- db/adapter_selected.py
+++ db/adapter_selected.py
@@ -1,11 +1,10 @@
-
-from mongoAdapter import MongoAdapter
-from sqlAdapter import MySQLAdapter
 import os
 
 db_seleccionada = os.getenv('TIPO_BASE_DE_DATOS', 'MySQL')
 
 if db_seleccionada=='MongoDB':
+	from mongoAdapter import MongoAdapter
 	adapter = MongoAdapter()
 else:
+	from sqlAdapter import MySQLAdapter
 	adapter = MySQLAdapter()
