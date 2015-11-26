cab94aea464da473d39e0bf1b8fb7edfba508c8a
Tatiana <tatianamolinari90@gmail.com>
2015-11-26 17:15:04 -0300

    cambiando homecontroller

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, make_response
 from flask.ext.login import LoginManager, login_user , logout_user , current_user , login_required, current_user
 from flask_restful import Resource
-from db.models import Usuario, Evento
 from formularios.evento_form import EventoForm
 from db.adapter_selected import adapter
 from flask import request, redirect, url_for, session
